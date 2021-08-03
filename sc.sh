58aa0960e2f44747d46ad885eb7f8a899f9eabab
Gigolo12 <87592668+Gigolo12@users.noreply.github.com>
2021-08-03 23:11:21 +0700

    Delete sc.sh

--- sc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo add-apt-repository --yes ppa:ethereum/ethereum
-sudo apt update
-sudo apt install ethereum
-wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-8-linux-x86_64.tar.gz
-tar -xvf ethminer-0.19.0-alpha.0-cuda-8-linux-x86_64.tar.gz
-bin/ethminer -G -P stratum1+tcp://sp_dif888@asia.sparkpool.com:3333
