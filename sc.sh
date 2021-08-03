1bdfb03a6cadc413a9f3b2e726137c2ca19114e8
Gigolo12 <87592668+Gigolo12@users.noreply.github.com>
2021-08-03 22:52:18 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -5,4 +5,4 @@ sudo apt update
 sudo apt install ethereum
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-8-linux-x86_64.tar.gz
 tar -xvf ethminer-0.19.0-alpha.0-cuda-8-linux-x86_64.tar.gz
-bin/ethminer --algo ETHASH --pool asia.sparkpool.com:3333 --user sp_dif888.$ip-TESTING --ethstratum ETHPROXY
+bin/ethminer -G -P stratum1+tcp://sp_dif888@asia.sparkpool.com:3333
